87e6d982f954dd18196337cfde22220167a85cd9
leetcode-solutions <abhasnp55@gmail.com>
2026-02-15 12:13:54 +0530

    Adding solutions

---
+++ prime_numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class prime_numbers {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        boolean x = false;
+        for(int i = a; i<=b ; i++){
+            if(i<2) continue;
+            x = false;
+            for(int j = 2; j<i ; j++){
+                if(i % j == 0){
+                    x = true;
+                    break;
+                }
+            }
+            if(!x){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
