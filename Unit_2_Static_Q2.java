588e7c4fea48457508625c64f5affab9d98c5299
leetcode-solutions <abhasnp55@gmail.com>
2026-02-23 03:50:09 +0530

    Adding solution of Unit_2_Static_Q2

---
+++ Unit_2_Static_Q2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Unit_2_Static_Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        
+        
+        int sum = 0;
+        for(int i = 0; i<n ; i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        
+        int count = sum;
+        for(int i =0; i<n ; i++){
+            if( count - arr[i] < arr[i]){
+                System.out.print(arr[i] + " ");
+            }
+            count = count - arr[i];
+        }
+    }
+}
