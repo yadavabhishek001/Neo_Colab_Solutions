9a55450a677e2cfaf33ca8ea7e6f9175a2a37e1d
leetcode-solutions <abhasnp55@gmail.com>
2026-03-31 20:12:59 +0530

    Added solution of Unit4_Lec25_Q4

---
+++ Unit4_Lec25_Q4.java
@@ -0,0 +1,8 @@
+import java.time.LocalTime;
+
+class LocalTimeExample {
+    public static void main(String[] args) {
+        LocalTime timeNow = LocalTime.now();
+        System.out.println(timeNow);
+    }
+}
