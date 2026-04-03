8ea9e1c4e8f7daf0208b2111f9f2c6b7d50d35a3
leetcode-solutions <abhasnp55@gmail.com>
2026-04-03 21:22:18 +0530

    Added solution of Unit4_Lec25_Q6

---
+++ Unit4_Lec25_Q6.java
@@ -0,0 +1,8 @@
+import java.time.ZonedDateTime;
+
+class ZonedDateTimeExample {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}
