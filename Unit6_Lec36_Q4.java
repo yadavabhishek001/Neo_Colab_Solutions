9f56e5be721b0ec339842fb2bc75e29b92d7d3fc
leetcode-solutions <abhasnp55@gmail.com>
2026-05-07 22:53:09 +0530

    Added solution of Unit6_Lec36_Q4

---
+++ Unit6_Lec36_Q4.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Unit6_Lec36_Q4 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        nums.add(10);
+        nums.add(20);
+
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+}
