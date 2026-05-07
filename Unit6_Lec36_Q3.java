0bee9945708b8344cbaa17579185f6e1a579f813
leetcode-solutions <abhasnp55@gmail.com>
2026-05-07 22:50:08 +0530

    Added solution of Unit6_Lec36_Q3

---
+++ Unit6_Lec36_Q3.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Unit6_Lec36_Q3 {
+    public static void main(String[] args) {
+
+        ArrayList<String> colors = new ArrayList<>();
+
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+
+        System.out.println("First color: " + colors.get(0));
+    }
+}
